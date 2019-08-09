6711d20d3ec1dac3cb8aba32884b7dfa92e07155
Dan Reeves <djreeves1292@gmail.com>
2019-08-09 23:01:00 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -12,5 +12,10 @@ def link
   puts "It's Dangerous To Go Alone! Take This."
   return "It's Dangerous To Go Alone! Take This."
 
+end
 
+def any_phrase(phrase)
+  phrase = "Do A Barrel Roll!"
+  puts phrase
+  return phrase
 end
